a2b10ac89675c11c6fed6b222a313ee9f4758e16
Ackermann Yuriy <ackermann.yuriy@gmail.com>
2019-02-08 13:47:13 +0000

    Added GetTransactions

--- main.go
+++ main.go
@@ -10,29 +10,71 @@ import (
     "bytes"
     "io/ioutil"
     "net/http"
+    "sync"
     // "strings"
-    // "strconv"
+    "strconv"
     "github.com/go-kit/kit/endpoint"
     "github.com/gorilla/mux"
     httptransport "github.com/go-kit/kit/transport/http"
 )
 
 const gethUrl string = "http://localhost:8545"
+var wg sync.WaitGroup
+
 // StringService provides operations on strings.
 type EthService interface {
     // Uppercase(string) (string, error)
     // Count(string) int
     GetSyncStatus() (interface{}, error)
+    GetTransactions(string) (interface{}, error)
 }
 
 /* ----- INTERFACE IMPLEMENTORS ----- */
-
 type EthRPCRequest struct {
     Jsonrpc string `json:"jsonrpc"`
     Method string `json:"method"`
     Params []string `json:"params"`
     Id int32 `json:"id"`
 }
+
+type BlockTransactionCount struct {
+    Jsonrpc string `json:"jsonrpc"`
+    Result string `json:"result"`
+    Id int32 `json:"id"`
+}
+
+type Transaction struct {
+    BlockHash string `json:"blockHash"`
+    BlockNumber string `json:"blockNumber"`
+    From string `json:"from"`
+    Gas string `json:"gas"`
+    GasPrice string `json:"gasPrice"`
+    Hash string `json:"hash"`
+    Input string `json:"input"`
+    nonce string `json:"nonce"`
+    To string `json:"to"`
+    TransactionIndex string `json:"transactionIndex"`
+    Value string `json:"value"`
+    V string `json:"v"`
+    R string `json:"r"`
+    S string `json:"s"`
+}
+
+type TransactionResult struct {
+    Jsonrpc string `json:"jsonrpc"`
+    Result Transaction `json:"result"`
+    Id int32 `json:"id"`
+}
+
+type TransactionResultsResponse struct {
+    Transactions []Transaction `json:"transactions"`
+}
+
+type TxChannelResult struct {
+    Tx Transaction
+    Error error
+}
+
 
 func (ethreq *EthRPCRequest) constructGetSyncingRequest() {
     ethreq.Jsonrpc = "2.0"
@@ -41,38 +83,26 @@ func (ethreq *EthRPCRequest) constructGetSyncingRequest() {
     ethreq.Id = 0x01
 }
 
-// func (ethreq *EthRPCRequest) constructGetBlockTransactionCountByHashRequest(blockHash string) {
-//     ethreq.Jsonrpc = "2.0"
-//     ethreq.Method = ""
-//     ethreq.Params = []string{blockHash}
-//     ethreq.Id = 0x01    
-// }
+func (ethreq *EthRPCRequest) constructGetBlockTransactionCountByHashRequest(blockHash string) {
+    ethreq.Jsonrpc = "2.0"
+    ethreq.Method = "eth_getBlockTransactionCountByHash"
+    ethreq.Params = []string{blockHash}
+    ethreq.Id = 0x01    
+}
 
-// func (ethreq *EthRPCRequest) constructGetTransactionByBlockHashAndIndexRequest(blockHash string, transactionIndex int) {
-//     trInx := '0x' + strconv.FormatInt(transactionIndex, 16)
-//     ethreq.Jsonrpc = "2.0"
-//     ethreq.Method = ""
-//     ethreq.Params = []string{blockHash, ethreq}
-//     ethreq.Id = 0x01
-// }
+func (ethreq *EthRPCRequest) constructGetTransactionByBlockHashAndIndexRequest(blockHash string, transactionIndex int64) {
+    var trInx string = "0x" + strconv.FormatInt(transactionIndex, 16)
+    ethreq.Jsonrpc = "2.0"
+    ethreq.Method = "eth_getTransactionByBlockHashAndIndex"
+    ethreq.Params = []string{blockHash, trInx}
+    ethreq.Id = 0x01
+}
 
-// ethServiceImp is a concrete implementation of StringService
-type ethServiceImp struct{}
-
-var ErrConnectingToGeth = errors.New("Error connecting to geth!")
-var ErrReadingGethResponse = errors.New("Error reading response from Geth!")
-
-func (ethServiceImp) GetSyncStatus() (interface{}, error) {
-    rpcReq := EthRPCRequest{}
-    fmt.Println(rpcReq)
-
-    rpcReq.constructGetSyncingRequest()
-
+func callGethRPC (rpcStruct EthRPCRequest) (interface{}, error) {
     var jsonData []byte
-    jsonData, err := json.Marshal(rpcReq)
+    jsonData, err := json.Marshal(rpcStruct)
 
-    fmt.Println(rpcReq)
-    fmt.Println(string(jsonData))
+    fmt.Println("JSON Request", string(jsonData))
     resp, err := http.Post(gethUrl, "application/json", bytes.NewBuffer(jsonData))
     if err != nil {
         return nil, ErrConnectingToGeth
@@ -82,11 +112,101 @@ func (ethServiceImp) GetSyncStatus() (interface{}, error) {
     if err != nil {
         return nil, ErrReadingGethResponse
     }
+    resp.Body.Close()
 
-    // fmt.Println("response Body:", resp.RawText())
+    fmt.Println("JSON Response", string(respBytes))
     return respBytes, nil
 }
 
+// ethServiceImp is a concrete implementation of StringService
+type ethServiceImp struct{}
+
+var ErrConnectingToGeth = errors.New("Error connecting to geth!")
+var ErrReadingGethResponse = errors.New("Error reading response from Geth!")
+var ErrParsingJSON = errors.New("Error while parsing JSON!")
+var ErrParsingInt = errors.New("Error while parsing JSON!")
+
+func (ethServiceImp) GetSyncStatus() (interface{}, error) {
+    rpcReq := EthRPCRequest{}
+
+    rpcReq.constructGetSyncingRequest()
+
+    resp, err := callGethRPC(rpcReq)
+    if err != nil {
+        return nil, err
+    }
+    
+    return resp, nil
+}
+
+func (ethServiceImp) GetTransactions(blockHash string) (interface{}, error) {
+    rpcReq := EthRPCRequest{}
+
+    // Getting transaction count
+    rpcReq.constructGetBlockTransactionCountByHashRequest(blockHash)
+    transactionCountResp, err := callGethRPC(rpcReq)
+    if err != nil {
+        return nil, err
+    }
+
+    var txCountStruct BlockTransactionCount;
+    err = json.Unmarshal(transactionCountResp.([]byte), &txCountStruct)
+    if err != nil {
+        return nil, ErrParsingJSON
+    }
+
+    fmt.Println(txCountStruct.Result)
+    
+    txCount, _ := strconv.ParseInt(txCountStruct.Result, 0, 16)
+    if err != nil {
+        return nil, ErrParsingInt
+    }
+
+    txChannel := make(chan TxChannelResult)
+    for i := 0; i < int(txCount); i++ {
+        wg.Add(1)
+        go func() {
+            defer wg.Done()
+            rpcReq := EthRPCRequest{}
+
+            // Getting transaction count
+            rpcReq.constructGetTransactionByBlockHashAndIndexRequest(blockHash, int64(i))
+            txBlockCountResp, err := callGethRPC(rpcReq)
+            if err != nil {
+                txChannel <- TxChannelResult{Transaction{}, err}
+            }
+
+            var txBlockCountRespStruct TransactionResult;
+            err = json.Unmarshal(txBlockCountResp.([]byte), &txBlockCountRespStruct)
+            if err != nil {
+                txChannel <- TxChannelResult{Transaction{}, err}
+            }
+
+            txChannel <- TxChannelResult{txBlockCountRespStruct.Result, nil}
+        }()
+    }
+
+    var txs []Transaction = []Transaction{}
+    for txResult := range txChannel {
+        if txResult.Error != nil {
+            return nil, txResult.Error
+        }
+        txs = append(txs, txResult.Tx)
+    }
+
+    wg.Wait()
+    close(txChannel)
+
+    txResponse := TransactionResultsResponse{txs}
+
+    var jsonData []byte
+    jsonData, err = json.Marshal(txResponse)
+    if err != nil {
+        return nil, ErrParsingJSON
+    }
+
+    return jsonData, nil
+}
 
 type addressResponse struct {
     A string `json:"address"`
@@ -124,15 +244,27 @@ func getSyncStatusConstr(svc EthService) endpoint.Endpoint {
     }
 }
 
+
+func getTransactionsConstr(svc EthService) endpoint.Endpoint {
+    return func(_ context.Context, request interface{}) (interface{}, error) {
+        resp, err := svc.GetTransactions(request.(string))
+        if err != nil {
+            return nil, err
+        }
+
+        return resp, nil
+    }
+}
+
 // Transports expose the service to the network. In this first example we utilize JSON over HTTP.
 func main() {
     svc := ethServiceImp{}
 
-    // addressHandler := httptransport.NewServer(
-    //     getAddressBack(svc),
-    //     decodeAddressRequest,
-    //     encodeResponse,
-    // )
+    addressHandler := httptransport.NewServer(
+        getTransactionsConstr(svc),
+        decodeAddressRequest,
+        encodeTransactionsResponse,
+    )
 
     getSyncHandler := httptransport.NewServer(
         getSyncStatusConstr(svc),
@@ -141,7 +273,7 @@ func main() {
     )
 
     router := mux.NewRouter()
-    // router.Methods("GET").PathPrefix("/getAddressTransactions/{address}").Handler(addressHandler)
+    router.Methods("GET").PathPrefix("/getBlockAddressTransactions/{blockHash}").Handler(addressHandler)
     router.Methods("GET").PathPrefix("/getSyncStatus/").Handler(getSyncHandler)
         
 
@@ -158,8 +290,8 @@ func main() {
 
 func decodeAddressRequest(_ context.Context, r *http.Request) (interface{}, error){
     vars := mux.Vars(r)
-    fmt.Println("Category: %v\n", vars["address"])
-    return vars["address"], nil
+    fmt.Println("BlockHash: ", vars["blockHash"])
+    return vars["blockHash"], nil
 }
 
 func decodeGetSyncRequest(_ context.Context, r *http.Request) (interface{}, error){
@@ -170,6 +302,10 @@ func encodeResponse(_ context.Context, w http.ResponseWriter, response interface
     return json.NewEncoder(w).Encode(response)
 }
 
+func encodeTransactionsResponse(_ context.Context, w http.ResponseWriter, response interface{}) error {
+    _, err := w.Write(response.([]byte))
+    return err
+}
 
 func encodeResponseGetSync(_ context.Context, w http.ResponseWriter, response interface{}) error {
     _, err := w.Write(response.([]byte))
